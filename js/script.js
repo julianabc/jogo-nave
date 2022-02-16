26a8814c39d6bf49c601a50466a8848aa2c8535e
julianabc <julianabc@id.uff.br>
2022-02-16 12:37:54 -0300

    acerto das teclas

--- js/script.js
+++ js/script.js
@@ -14,14 +14,33 @@ function start() {
 
 	// funções para movimentaçao do fundo do jogo
 
-	let jogo = {}
-	
-	jogo.timer = setInterval(loop,30); // chama a função loop a cada 30ms
-	
+	let jogo = {} // declaração do objeto jogo
+	jogo.pressionou = []; // array para as teclas que foram pressionadas
+
 	function loop() {
 		movefundo();
+		movejogador();
 	
 	} // Fim da função loop()
+
+	// numeros decimais das teclas do jogo
+	const TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+	}
+
+	//Verifica se o usuário pressionou alguma tecla	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+	});
+	
+	$(document).keyup(function(e){
+		jogo.pressionou[e.which] = false;
+	});
+
+	
+	jogo.timer = setInterval(loop,30); // chama a função loop a cada 30ms
 
 	function movefundo() {
 		
@@ -32,6 +51,38 @@ function start() {
 		$("#fundoGame").css("background-position",esquerda-1); 
 	
 	} // fim da função movefundo()
+
+	// verifica se as teclas foram pressionadas para realizar a ação
+	function movejogador() {
+		
+		if (jogo.pressionou[TECLA.W]) {
+			let topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+
+			// impede que ultrapasse o quadrado
+			if (topo<=0) {
+				$("#jogador").css("top",topo+10);
+			}
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {
+			let topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);
+
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+					
+			}
+			
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			
+			//Chama função Disparo	
+		}
+	
+	} // fim da função movejogador()
+	
 
 
 
